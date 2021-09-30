f2411533cfeb7c3cd1ba3f01fb6e116c57187a0e
George Dobrovolsky <georgy.dobrovolsky@gmail.com>
2021-09-30 13:07:55 +0300

    kubebuilder scaffold, adjusted

---
+++ api/v1/envoyfleet_types.go
@@ -0,0 +1,64 @@
+/*
+Copyright 2021.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// EnvoyFleetSpec defines the desired state of EnvoyFleet
+type EnvoyFleetSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Foo is an example field of EnvoyFleet. Edit envoyfleet_types.go to remove/update
+	Foo string `json:"foo,omitempty"`
+}
+
+// EnvoyFleetStatus defines the observed state of EnvoyFleet
+type EnvoyFleetStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// EnvoyFleet is the Schema for the envoyfleet API
+type EnvoyFleet struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   EnvoyFleetSpec   `json:"spec,omitempty"`
+	Status EnvoyFleetStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// EnvoyFleetList contains a list of EnvoyFleet
+type EnvoyFleetList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []EnvoyFleet `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&EnvoyFleet{}, &EnvoyFleetList{})
+}
---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/kubeshop/kusk-gateway
+
+go 1.16
+
+require (
+	github.com/envoyproxy/go-control-plane v0.9.9
+	github.com/golang/protobuf v1.4.3
+	github.com/onsi/ginkgo v1.14.1
+	github.com/onsi/gomega v1.10.2
+	k8s.io/apimachinery v0.20.2
+	k8s.io/client-go v0.20.2
+	sigs.k8s.io/controller-runtime v0.8.3
+)
