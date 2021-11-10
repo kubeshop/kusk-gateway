9b9ebb2c7574b1b80db55a4fd93c15cd0665d1b8
Taras Yatsurak <tyatsurak@gmail.com>
2021-11-10 11:53:33 +0200

    Adds sorting of routes

--- controllers/envoyfleet_controller.go
+++ controllers/envoyfleet_controller.go
@@ -41,11 +41,6 @@ type EnvoyFleetReconciler struct {
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the EnvoyFleet object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.8.3/pkg/reconcile
 func (r *EnvoyFleetReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
