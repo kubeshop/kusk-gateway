06a98097e805098986bdb897d1c203071537e0c1
Kyle Hodgetts <kyle@kubeshop.io>
2021-12-17 11:10:56 +0200

    start adding tests

---
+++ envoy/headermatcher_test.go
@@ -0,0 +1,29 @@
+package envoy
+
+import (
+	"reflect"
+	"testing"
+
+	route "github.com/envoyproxy/go-control-plane/envoy/config/route/v3"
+)
+
+func TestGetHeaderMatcherConfig(t *testing.T) {
+	type args struct {
+		methods []string
+		cors    bool
+	}
+	tests := []struct {
+		name string
+		args args
+		want *route.HeaderMatcher
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := GetHeaderMatcherConfig(tt.args.methods, tt.args.cors); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GetHeaderMatcherConfig() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
