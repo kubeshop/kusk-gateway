78029bc3362e003ade8fb7aeb8bc842e26097af8
Taras Yatsurak <Tarick@users.noreply.github.com>
2022-03-07 16:42:24 +0200

    Speed Up local development (#245)
    * Speedup local development with Docker images and GOPROXY caches

--- development/cluster/create-env.sh
+++ development/cluster/create-env.sh
@@ -2,23 +2,14 @@
 
 set -e
 
-if ! command -v jq &>/dev/null; then
-  echo "jq could not be found"
-  exit
-fi
-
 echo "========> creating cluster..."
-minikube start --profile kgw
+minikube start --profile kgw --addons=metallb
 
 # determine load balancer ingress range
 cidr_base_addr=$(minikube ip --profile kgw)
 ingress_first_addr=$(echo "$cidr_base_addr" | awk -F'.' '{print $1,$2,$3,2}' OFS='.')
 ingress_last_addr=$(echo "$cidr_base_addr" | awk -F'.' '{print $1,$2,$3,255}' OFS='.')
 ingress_range=$ingress_first_addr-$ingress_last_addr
-
-# deploy metallb
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.11.0/manifests/namespace.yaml
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.11.0/manifests/metallb.yaml
 
 # configure metallb ingress address range
 cat <<EOF | kubectl apply -f -
@@ -42,8 +33,7 @@ make install
 echo "========> building control-plane docker image and installing into cluster"
 
 SHELL=/bin/bash
-eval $(minikube docker-env --profile "kgw")
-make docker-build deploy
+make docker-images-cache docker-build deploy
 
 kubectl rollout status -w deployment/kusk-gateway-manager -n kusk-system
 
