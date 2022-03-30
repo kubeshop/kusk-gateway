e48d4dbe5f32d0b87478134a500eed1703d91894
Jasmin Gacic <jasmin.gacic@gmail.com>
2022-03-30 11:12:17 +0000

    various changes
    Signed-off-by: Jasmin Gacic <jasmin.gacic@gmail.com>

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -194,7 +194,7 @@ func main() {
 		"Enable leader election for controller manager. "+
 			"Enabling this will ensure there is only one active controller manager.")
 	flag.BoolVar(&development, "development", false, "enable development mode")
-	flag.StringVar(&logLevel, "log-level", "INFO", "level of log detail [DEBUG|INFO|WARN|ERROR|DPANIC|PANIC|FATAL]")
+	flag.StringVar(&logLevel, "log-level", "WARN", "level of log detail [DEBUG|INFO|WARN|ERROR|DPANIC|PANIC|FATAL]")
 	// This one is configurable for the local development
 	flag.StringVar(&webhookCertsDir, "webhook-certs-dir", "/opt/manager/webhook/certs", "The directory where webhook certificates will be generated.")
 
@@ -274,6 +274,16 @@ func main() {
 		setupLog.Info("Starting K8s secrets watch for the TLS certificates renewal events")
 		controllerConfigManager.WatchSecrets(ctx.Done())
 	}()
+
+	if err = (&controllers.ServiceReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.
+			WithValues("controller", "ServiceAnnotationReconciler").
+			Error(err, "Unable to create controller")
+		os.Exit(1)
+	}
 
 	// EnvoyFleet obj controller
 	if err = (&controllers.EnvoyFleetReconciler{
---
+++ internal/controllers/service_controller.go
@@ -0,0 +1,146 @@
+/*
+Copyright 2022.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+
+	gateway "github.com/kubeshop/kusk-gateway/api/v1alpha1"
+
+	"gopkg.in/yaml.v2"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+)
+
+// ServiceReconciler reconciles a Pod object
+type ServiceReconciler struct {
+	client.Client
+	Scheme *runtime.Scheme
+}
+
+//+kubebuilder:rbac:groups=core,resources=pods,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=core,resources=pods/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=core,resources=pods/finalizers,verbs=update
+func (r *ServiceReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	l := log.FromContext(ctx).WithName("service-reconciler")
+
+	l.Info("Reconciling changed Service resource", "changed", req.NamespacedName)
+
+	svc := &corev1.Service{}
+	if err := r.Client.Get(ctx, client.ObjectKey{Namespace: req.Namespace, Name: req.Name}, svc); err != nil {
+		if errors.IsNotFound(err) {
+			return ctrl.Result{}, nil
+		}
+		return ctrl.Result{}, err
+	}
+
+	if val, ok := svc.Annotations["kusk-gateway/openapi-url"]; ok {
+		l.Info(`Detected "kusk-gateway/openapi-url" annotation`, "found", val)
+		openapi, err := getOpenAPIfromURL(val)
+		if err != nil {
+			return ctrl.Result{}, err
+		}
+
+		var yml map[string]interface{}
+		err = yaml.Unmarshal(openapi, &yml)
+		if err != nil {
+			return ctrl.Result{}, err
+		}
+
+		if _, ok := yml["x-kusk"]; !ok {
+			l.Info(`"x-kusk" extension not found in OpenAPI definition`, "missing", val)
+
+			service := map[string]interface{}{"service": map[string]interface{}{
+				"name":      req.Name,
+				"namespace": req.Namespace,
+				"port":      svc.Spec.Ports[0].Port,
+			}}
+			upstream := map[string]interface{}{
+				"upstream": service,
+			}
+			yml["x-kusk"] = upstream
+		}
+
+		yamlPayload, err := yaml.Marshal(yml)
+		if err != nil {
+			return ctrl.Result{}, err
+		}
+
+		gatewaySpec := gateway.APISpec{
+			Spec: string(yamlPayload),
+		}
+
+		fmt.Println("payload", gatewaySpec.Spec)
+		api := &gateway.API{}
+
+		if err := r.Client.Get(ctx, client.ObjectKey{Namespace: req.Namespace, Name: req.Name}, api); err != nil {
+			if errors.IsNotFound(err) {
+				//create
+				api.Name = req.Name
+				api.Namespace = req.Namespace
+				api.Spec = gatewaySpec
+				if err := r.Client.Create(ctx, api, &client.CreateOptions{}); err != nil {
+					l.Error(err, "error occured while creating API")
+					return ctrl.Result{}, err
+				}
+
+			}
+		} else {
+			api.Spec = gatewaySpec
+			if err := r.Client.Update(ctx, api, &client.UpdateOptions{}); err != nil {
+				l.Error(err, "error occured while updating API")
+				return ctrl.Result{}, err
+
+			}
+		}
+
+	}
+
+	return ctrl.Result{}, nil
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *ServiceReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	fmt.Println("SCREEEEEEEAM!!!!!!")
+
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&corev1.Service{}).
+		Complete(r)
+}
+
+func getOpenAPIfromURL(url string) ([]byte, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	b, err := io.ReadAll(resp.Body)
+	// b, err := ioutil.ReadAll(resp.Body)  Go.1.15 and earlier
+	if err != nil {
+		return nil, err
+	}
+
+	return b, err
+}
