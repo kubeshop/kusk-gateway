62f75e8ffbedbf7c3681906acbeb0f21cce19f31
Kyle Hodgetts <kyle@kubeshop.io>
2022-04-28 10:10:37 +0100

    add 2022 MIT license to all go files (#340)

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,3 +1,26 @@
+/*
+MIT License
+
+Copyright (c) 2022 Kubeshop
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
 package main
 
 import (
@@ -8,14 +31,15 @@ import (
 	"os"
 	"time"
 
-	"github.com/kubeshop/kusk-gateway/internal/agent/httpserver"
-	"github.com/kubeshop/kusk-gateway/internal/agent/management"
-	"github.com/kubeshop/kusk-gateway/pkg/analytics"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/keepalive"
+
+	"github.com/kubeshop/kusk-gateway/internal/agent/httpserver"
+	"github.com/kubeshop/kusk-gateway/internal/agent/management"
+	"github.com/kubeshop/kusk-gateway/pkg/analytics"
 )
 
 func main() {
