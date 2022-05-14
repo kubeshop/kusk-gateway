aeade3a1387c21450604505b5c8f48823977be09
Jasmin Gacic <jasmin.gacic@gmail.com>
2022-05-14 01:31:00 +0000

    reverted to GA4
    Signed-off-by: Jasmin Gacic <jasmin.gacic@gmail.com>

--- pkg/analytics/analytics.go
+++ pkg/analytics/analytics.go
@@ -24,59 +24,132 @@ SOFTWARE.
 package analytics
 
 import (
+	"bytes"
 	"crypto/md5"
 	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
 	"os"
-	"time"
+	"strings"
 
 	"github.com/denisbrodbeck/machineid"
-	"github.com/segmentio/analytics-go"
+)
+
+const (
+	gaUrl           = "https://www.google-analytics.com/mp/collect?measurement_id=%s&api_secret=%s"
+	gaValidationUrl = "https://www.google-analytics.com/debug/mp/collect?measurement_id=%s&api_secret=%s"
 )
 
 var (
-	TelemetryToken = "" // value needs to be passed with LDFLAG set to github.com/kubeshop/kusk-gateway/pkg/analytics.TelemetryToken
+	KuskGAMeasurementID = "" // value needs to be passed with LDFLAG set to github.com/kubeshop/kusk-gateway/pkg/analytics.KuskGAMeasurementID
+	KuskGAApiSecret     = "" // value needs to be passed with LDFLAG set to github.com/kubeshop/kusk-gateway/pkg/analytics.KuskGAApiSecret
 )
 
 func SendAnonymousInfo(event string) error {
-	track := analytics.Track{
-		AnonymousId: MachineID(),
-		Event:       "kusk-heartbeat",
-		Properties:  analytics.NewProperties().Set("event", event),
-		Timestamp:   time.Now(),
+	payload := Payload{
+		ClientID: MachineID(),
+		Events: []Event{
+			{
+				Name: event,
+				Params: Params{
+					EventCount:       1,
+					EventCategory:    "api-request",
+					AppName:          "kusk-gateway",
+					CustomDimensions: event,
+					DataSource:       "gateway",
+				},
+			},
+		},
 	}
-
-	return sendDataToGA(track)
+	return sendDataToGA(payload)
 }
 
 // SendAnonymouscmdInfo will send CLI event to GA
-func SendAnonymousCMDInfo() {
-	var event string
+func SendAnonymousCMDInfo() error {
+	event := "command"
 	command := []string{}
 	if len(os.Args) > 1 {
 		command = os.Args[1:]
 		event = command[0]
 	}
 
-	track := analytics.Track{
-		AnonymousId: MachineID(),
-		UserId:      MachineID(),
-		Event:       "kusk-cli",
-		Properties:  analytics.NewProperties().Set("event", event),
+	payload := Payload{
+		ClientID: MachineID(),
+		Events: []Event{
+			{
+				Name: event,
+				Params: Params{
+					EventCount:       1,
+					EventCategory:    "beacon",
+					AppName:          "kusk-cli",
+					CustomDimensions: strings.Join(command, " "),
+				},
+			}},
 	}
-	sendDataToGA(track)
-
+	return sendDataToGA(payload)
 }
 
-func sendDataToGA(track analytics.Track) error {
+func sendDataToGA(data Payload) error {
 	// if environment variable is set return and collect nothing
 	if _, ok := os.LookupEnv("KUSK_ANALYTICS_DISABLED"); ok {
 		return nil
 	}
+	sendValidationRequest(data)
 
-	client := analytics.New(TelemetryToken)
-	defer client.Close()
-	return client.Enqueue(track)
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
 
+	request, err := http.NewRequest("POST", fmt.Sprintf(gaUrl, KuskGAMeasurementID, KuskGAApiSecret), bytes.NewBuffer(jsonData))
+	if err != nil {
+		return err
+	}
+
+	request.Header.Set("Content-Type", "application/json")
+
+	resp, err := http.DefaultClient.Do(request)
+	if err != nil {
+		return err
+	}
+	o, _ := io.ReadAll(resp.Body)
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode > 300 {
+		return fmt.Errorf("could not POST, statusCode: %d, body: %s", resp.StatusCode, string(o))
+	}
+	return nil
+}
+
+func sendValidationRequest(payload Payload) (out string, err error) {
+
+	jsonData, err := json.Marshal(payload)
+	if err != nil {
+		return out, err
+	}
+
+	request, err := http.NewRequest("POST", fmt.Sprintf(gaValidationUrl, KuskGAMeasurementID, KuskGAApiSecret), bytes.NewBuffer(jsonData))
+	if err != nil {
+		return out, err
+	}
+
+	request.Header.Set("Content-Type", "application/json")
+
+	resp, err := http.DefaultClient.Do(request)
+	if err != nil {
+		return out, err
+	}
+	defer resp.Body.Close()
+	b, err := ioutil.ReadAll(resp.Body)
+
+	if resp.StatusCode > 300 {
+		return out, fmt.Errorf("could not POST, statusCode: %d", resp.StatusCode)
+	}
+	return fmt.Sprintf("status: %d - %s", resp.StatusCode, b), err
 }
 
 type Params struct {
