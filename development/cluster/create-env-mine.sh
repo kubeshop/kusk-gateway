770b74fa920c9dc0a36dd4cf1061ed8ea91ab101
Mohamed Bana <mohamed@bana.io>
2022-05-20 00:16:03 +0100

    Create `kind` cluster - https://kind.sigs.k8s.io
    Signed-off-by: Mohamed Bana <mohamed@bana.io>

---
+++ development/cluster/create-env-mine.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+set -e
+
+echo "========> creating cluster..."
+minikube start --profile kgw --addons=metallb
+
+# determine load balancer ingress range
+cidr_base_addr=$(minikube ip --profile kgw)
+ingress_first_addr=$(echo "$cidr_base_addr" | awk -F'.' '{print $1,$2,$3,2}' OFS='.')
+ingress_last_addr=$(echo "$cidr_base_addr" | awk -F'.' '{print $1,$2,$3,255}' OFS='.')
+ingress_range=$ingress_first_addr-$ingress_last_addr
+
+# configure metallb ingress address range
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  namespace: metallb-system
+  name: config
+data:
+  config: |
+    address-pools:
+    - name: default
+      protocol: layer2
+      addresses:
+      - $ingress_range
+EOF
+
+# echo "========> installing CRDs"
+# make install
+
+# echo "========> building control-plane docker image and installing into cluster"
+
+# SHELL=/bin/bash
+# make docker-build deploy
+
+# kubectl rollout status -w deployment/kusk-gateway-manager -n kusk-system
+
+# echo "========> Deploying default Envoy Fleet"
+# make deploy-envoyfleet
--- development/cluster/kind/cluster-create.sh
+++ development/cluster/kind/cluster-create.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
 kind create cluster --image kindest/node:v1.24.0 --wait 5m --name kusk --config ./cluster.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.12.1/manifests/namespace.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.12.1/manifests/metallb.yaml
+kubectl apply -f https://kind.sigs.k8s.io/examples/loadbalancer/metallb-configmap.yaml
+ANALYTICS_ENABLED=false kusk install --no-api --no-dashboard --name=kusk --namespace=default
+kubectl get pods --namespace metallb-system --watch
+kind delete cluster --name kusk
