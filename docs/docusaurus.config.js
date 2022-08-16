5a9db37ac1cfb7a99afe9c2dbb93bcb7286a2af8
Abdallah Abedraba <aabedraba@gmail.com>
2022-08-16 17:39:33 +0200

    update path for docs to be root

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -37,6 +37,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl: "https://github.com/kubeshop/kusk-gateway",
+          routeBasePath: "/"
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
