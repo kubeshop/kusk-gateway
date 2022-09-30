aa45b7777804b31213829d835cd7b895ddd4c455
jasmingacic <jasmin.gacic@gmail.com>
2022-09-30 14:12:42 +0000

    handling lack of public IP and skipping version check for 'kusk ip'
    Signed-off-by: jasmingacic <jasmin.gacic@gmail.com>

--- cmd/kusk/cmd/ip.go
+++ cmd/kusk/cmd/ip.go
@@ -110,16 +110,18 @@ var ipCmd = &cobra.Command{
 				}
 			}
 		}
-		if ip == "" {
-			//kubectl port-forward svc/kusk-gateway-dashboard -n kusk-system 8080:80
-			if svc.Spec.Type == "ClusterIP" {
-				err := fmt.Errorf("your envoyfleet doesn't have IP address assigned. Try portforwarding %q", fmt.Sprintf("kubectl port-forward svc/%s  -n %s 8080:%d", svc.Name, svc.Namespace, svc.Spec.Ports[0].Port))
-				reportError(err)
-			} else {
-				err := fmt.Errorf("your envoyfleet doesn't have IP address assigned yet retry or try portforwarding %q", fmt.Sprintf("kubectl port-forward svc/%s  -n %s 8080:%d", svc.Name, svc.Namespace, svc.Spec.Ports[0].Port))
-				reportError(err)
-			}
+		//kubectl port-forward svc/kusk-gateway-dashboard -n kusk-system 8080:80
+		if svc.Spec.Type == "ClusterIP" {
+			err := fmt.Errorf("your envoyfleet doesn't have public IP address assigned. Try portforwarding %q", fmt.Sprintf("kubectl port-forward svc/%s  -n %s 8080:%d", svc.Name, svc.Namespace, svc.Spec.Ports[0].Port))
+			reportError(err)
+			return err
 		}
+		if ip == "" {
+			err := fmt.Errorf("your envoyfleet doesn't have public IP address assigned yet retry or try portforwarding %q", fmt.Sprintf("kubectl port-forward svc/%s  -n %s 8080:%d", svc.Name, svc.Namespace, svc.Spec.Ports[0].Port))
+			reportError(err)
+			return err
+		}
+
 		fmt.Println(ip)
 		return nil
 	},
