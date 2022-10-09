ea4423f4a66608c2c36606e0c48941e0b24c7ba0
Abdallah Abedraba <aabedraba@gmail.com>
2022-10-09 14:20:47 +0400

    remove global unused flags

--- cmd/kusk/cmd/root.go
+++ cmd/kusk/cmd/root.go
@@ -199,12 +199,8 @@ func init() {
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.kusk.yaml)")
-
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-	rootCmd.PersistentFlags().BoolVarP(&verbose, "verbose", "v", false, "Help message for toggle")
 	rootCmd.SetHelpFunc(help)
 }
 
