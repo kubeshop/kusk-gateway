014ae0862e6c724c48f069e1c724caccda970b83
Jasmin Gacic <jasmin.gacic@gmail.com>
2022-10-19 11:03:34 +0200

    added validate command (#882)
    Signed-off-by: jasmingacic <jasmin.gacic@gmail.com>
    
    Signed-off-by: jasmingacic <jasmin.gacic@gmail.com>

---
+++ cmd/kusk/cmd/validate.go
@@ -0,0 +1,64 @@
+/*
+The MIT License (MIT)
+
+# Copyright © 2022 Kubeshop
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/kubeshop/kusk-gateway/cmd/kusk/internal/errors"
+	"github.com/kubeshop/kusk-gateway/cmd/kusk/internal/kuskui"
+	"github.com/spf13/cobra"
+)
+
+var validateCmd = &cobra.Command{
+	Use:           "validate",
+	Short:         "Validates provided Open API definition",
+	SilenceUsage:  true,
+	SilenceErrors: true,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		reportError := func(err error) {
+			if err != nil {
+				errors.NewErrorReporter(cmd, err).Report()
+			}
+		}
+
+		_, err := getParsedAndValidatedOpenAPISpec(file)
+		if err != nil {
+			reportError(err)
+			return err
+		}
+
+		kuskui.PrintSuccess(fmt.Sprintf("successfully parsed %s", file))
+
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(validateCmd)
+
+	validateCmd.Flags().StringVarP(&file, "in", "i", "", "file path or URL to OpenAPI spec file to generate mappings from. e.g. --in apispec.yaml")
+	validateCmd.MarkFlagRequired("file")
+}
