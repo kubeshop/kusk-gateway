1c499fd40f716638ea8a8c906a202edccb346056
Jasmin Gacic <jasmin.gacic@gmail.com>
2022-10-19 23:30:51 +0200

    build fix (#891)
    * build fix
    
    Signed-off-by: jasmingacic <jasmin.gacic@gmail.com>
    
    * remove line break from println
    
    Signed-off-by: jasmingacic <jasmin.gacic@gmail.com>
    Co-authored-by: Abdallah Abedraba <aabedraba@gmail.com>

--- cmd/kusk/cmd/version.go
+++ cmd/kusk/cmd/version.go
@@ -114,7 +114,8 @@ func NewVersionCommand(writer io.Writer, version string) *cobra.Command {
 			}
 
 			if outdated {
-				fmt.Println(kuskui.Red("\nℹ️  Outdated Kusk version in your cluster."), "\nPlease run the following command to update your cluster \n\n kusk cluster upgrade\n")
+				fmt.Println(kuskui.Red("\nℹ️  Outdated Kusk version in your cluster."), "\nPlease run the following command to update your cluster \n\n kusk cluster upgrade")
+				fmt.Println()
 			}
 			return nil
 		},
