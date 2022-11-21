f4b8f98d81f46f80510816f7164b6f017fbfe983
jasmingacic <jasmin.gacic@gmail.com>
2022-11-21 12:00:54 +0000

    cleaned up overlays_test.go
    Signed-off-by: jasmingacic <jasmin.gacic@gmail.com>

--- cmd/kusk/internal/overlays/overlays_test.go
+++ cmd/kusk/internal/overlays/overlays_test.go
@@ -1,16 +1,12 @@
 package overlays
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestOverlay(t *testing.T) {
-	// o := &Overlay{}
-
-	//https://gist.githubusercontent.com/ponelat/daac5912ede1871629b6028bbe715d3a/raw/2871f9f27fb93d1c01567d198fb60cd1271e7dcf/overlay.yml
-	// fmt.Println(o.Parse("https://gist.githubusercontent.com/ponelat/daac5912ede1871629b6028bbe715d3a/raw/2871f9f27fb93d1c01567d198fb60cd1271e7dcf/overlay.yml"))
-
-	fmt.Println(applyOverlay("/root/go/src/github.com/kubeshop/kusk-gateway/cmd/kusk/internal/overlays/overlay.yaml", "/root/go/src/github.com/kubeshop/kusk-gateway/cmd/kusk/internal/overlays/petstore.yaml"))
-
+	if _, err := applyOverlay("overlay.yaml", ""); err != nil {
+		t.Log(err)
+		t.Fail()
+	}
 }
